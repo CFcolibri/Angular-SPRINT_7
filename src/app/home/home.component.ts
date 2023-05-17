5a63cfb1f96585eb281c65bdc25c97ab76db11f3
CFcolibri <carlotafigueras@gmail.com>
2023-05-17 16:57:29 +0200

    exercise1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,22 @@
+// import { Component } from '@angular/core';
+// import {FormBuilder} from '@angular/forms';
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.css']
+// })
+// export class HomeComponent {
+//   price = this._formBuilder.group({
+
+//   });
+
+//   constructor(private _formBuilder: FormBuilder) {
+// }
+// }
+
 import { Component } from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +24,36 @@ import {FormBuilder} from '@angular/forms';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  toppings = this._formBuilder.group({
-    pepperoni: false,
-    extracheese: false,
-    mushroom: false,
-  });
+  price: FormGroup;
+  totalPrice: number = 0;
 
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder) {
+    this.price = this.formBuilder.group({
+      web: false,
+      consoltoria: false,
+      campanya: false
+    });
+  }
+
+  calculateTotalPrice() {
+    this.totalPrice = 0;
+
+    if (this.price.get('web')?.value) {
+      this.totalPrice += 500;
+    }
+
+    if (this.price.get('consoltoria')?.value) {
+      this.totalPrice += 300;
+    }
+
+    if (this.price.get('campanya')?.value) {
+      this.totalPrice += 200;
+    }
+
+  }
+
+  onCheckboxChange() {
+    this.calculateTotalPrice();
+
+  }
 }
